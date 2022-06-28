040337cd4990544f9a19dbb8d603b13da498da36
You Matsumoto <youmts22@gmail.com>
2022-06-28 22:20:58 +0900

    refactor: one loop

--- src/slicer.rs
+++ src/slicer.rs
@@ -1,4 +1,5 @@
-use std::collections::LinkedList;
+use core::panic;
+use std::cmp::Ordering;
 use std::iter::*;
 
 pub fn slice<S, D>(src: Vec<S>, dest: Vec<D>) -> Vec<(S, D)>
@@ -6,56 +7,99 @@ where
     S: SliceItem<i32> + Copy + std::fmt::Debug,
     D: SliceItem<i32> + Copy + std::fmt::Debug,
 {
-    let mut src_ll = LinkedList::new();
-    for s in src {
-        src_ll.push_back(s)
-    }
-    let mut dest_ll = LinkedList::new();
-    for d in dest {
-        dest_ll.push_back(d)
-    }
+    let mut src_ret = Vec::new();
+    let mut dest_ret = Vec::new();
 
-    // TODO: a little slow algorithm
-    let mut x = 0i32;
-    for d in dest_ll.iter() {
-        x += d.get_key();
-        src_ll = slice_list(src_ll, x);
-    }
+    let mut src_iter = src.iter();
+    let mut dest_iter = dest.iter();
 
-    let mut x: i32 = 0i32;
-    for s in src_ll.iter() {
-        x += s.get_key();
-        dest_ll = slice_list(dest_ll, x);
+    let src_item = src_iter.next();
+    let dest_item = dest_iter.next();
+
+    if src_item.is_none() {
+        panic!("Src must not be empty array.")
+    }
+    if dest_item.is_none() {
+        panic!("Dest must not be empty array.")
     }
 
-    zip(src_ll.into_iter(), dest_ll.into_iter()).collect::<Vec<(S, D)>>()
-}
+    let mut src_item = Box::new(*src_item.unwrap());
+    let mut dest_item = Box::new(*dest_item.unwrap());
 
-pub fn slice_list<T>(list: LinkedList<T>, at: i32) -> LinkedList<T>
-where
-    T: SliceItem<i32> + Copy + std::fmt::Debug,
-{
-    let mut x: i32 = 0i32;
-    let mut result = LinkedList::new();
-    for item in list {
-        let v = item.get_key();
-        if x < at && at < x + v {
-            // NOTE: split element
-            let mut a = item;
-            *a.get_mut_key() = at - x;
-            result.push_back(a);
+    let mut src_x = 0;
+    let mut dest_x = 0;
 
-            let mut b = item;
-            *b.get_mut_key() = x + v - at;
-            result.push_back(b);
-        } else {
-            // NOTE: no split element
-            result.push_back(item);
+    loop {
+        let src_key = src_item.get_key();
+        let dest_key = dest_item.get_key();
+        let src_x_next = src_x + src_key;
+        let dest_x_next = dest_x + dest_key;
+
+        match src_x_next.cmp(&dest_x_next) {
+            Ordering::Greater => {
+                dest_ret.push(*dest_item);
+
+                src_ret.push({
+                    let mut left = *src_item;
+                    *left.get_mut_key() = dest_key;
+                    left
+                });
+
+                src_item = Box::new({
+                    let mut right = *src_item;
+                    *right.get_mut_key() = src_key - dest_key;
+                    right
+                });
+
+                src_x = dest_x_next;
+                dest_x = dest_x_next;
+
+                dest_item = match dest_iter.next() {
+                    Some(item) => Box::new(*item),
+                    None => break,
+                };
+            }
+            Ordering::Less => {
+                src_ret.push(*src_item);
+
+                dest_ret.push({
+                    let mut left = *dest_item;
+                    *left.get_mut_key() = src_key;
+                    left
+                });
+
+                dest_item = Box::new({
+                    let mut right = *dest_item;
+                    *right.get_mut_key() = dest_key - src_key;
+                    right
+                });
+
+                src_x = src_x_next;
+                dest_x = src_x_next;
+
+                src_item = match src_iter.next() {
+                    Some(item) => Box::new(*item),
+                    None => break,
+                };
+            }
+            Ordering::Equal => {
+                src_ret.push(*src_item);
+                dest_ret.push(*dest_item);
+
+                src_item = match src_iter.next() {
+                    Some(item) => Box::new(*item),
+                    None => break,
+                };
+
+                dest_item = match dest_iter.next() {
+                    Some(item) => Box::new(*item),
+                    None => break,
+                };
+            }
         }
-        x += item.get_key();
     }
 
-    result
+    zip(src_ret.into_iter(), dest_ret.into_iter()).collect::<Vec<(S, D)>>()
 }
 
 pub trait SliceItem<K> {
